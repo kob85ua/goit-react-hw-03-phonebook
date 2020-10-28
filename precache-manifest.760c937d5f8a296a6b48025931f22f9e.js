70519562a84c6774cf987f1c6f7a54fb50516aa8
M.Kobeliev <kob85ua@gmail.com>
2020-10-28 18:22:11 +0200

    Updates

---
+++ precache-manifest.760c937d5f8a296a6b48025931f22f9e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ec8f3115667e606f8522ec68da09828b",
+    "url": "/goit-react-hw-03-phonebook/index.html"
+  },
+  {
+    "revision": "7e58fdd4b58d48b44349",
+    "url": "/goit-react-hw-03-phonebook/static/js/2.49184181.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/goit-react-hw-03-phonebook/static/js/2.49184181.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1ea8664f74c8071bda30",
+    "url": "/goit-react-hw-03-phonebook/static/js/main.a05072a4.chunk.js"
+  },
+  {
+    "revision": "b806b48d21f1a8f90636",
+    "url": "/goit-react-hw-03-phonebook/static/js/runtime-main.58905eb4.js"
+  }
+]);
